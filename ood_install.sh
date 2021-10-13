2e49a509872b1638cce58276b3257547324827c9
Robin Karlsson <robin.karlsson@csc.fi>
2021-10-13 16:50:23 +0300

    Fix widget copying

--- ood_install.sh
+++ ood_install.sh
@@ -5,4 +5,4 @@ mkdir -p /ood-full/$1/config/ondemand.d
 rsync --inplace dashboard/ood.rb /ood-full/$1/custom_initializers/dashboard/ood.rb
 rsync locales/en.yml /ood-full/$1/locales/en.yml
 rsync ondemand.d/dashboard.yml /ood-full/$1/config/ondemand.d/dashboard.yml
-rsync widgets/ /ood-full/$1/widgets/
+rsync -a widgets/ /ood-full/$1/widgets/
