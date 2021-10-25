cc6cccbedb1658181b7036aeb8f9c40252467420
Robin Karlsson <robin.karlsson@csc.fi>
2021-10-25 15:06:32 +0300

    Add job exit reason parsing

--- dashboard/app_session_info.rb
+++ dashboard/app_session_info.rb
@@ -1,3 +1,5 @@
+require 'open3'
+
 # Class for outputting info about an interactive app session in info.html.erb
 # Usage eg. <%= CSCAppSessionInfo.log_link(self) %>
 class CSCAppSessionInfo
@@ -27,6 +29,58 @@ href="#{OodAppkit.files.url(path: session.output_file)}">output.log
       end
       msg
     end
+
+    # Returns the reason why a job exited
+    def exit_reason(session)
+      sanitizer ||= Rails::Html::FullSanitizer.new
+      if session.cache_completed
+        sanitizer.sanitize(get_exit_reason(session))
+      else
+        ""
+      end
+    end
+
+    private
+
+    # Get the cached exit reason or parse from log file if cached doesn't exist
+    def get_exit_reason(session)
+      log_file = session.output_file
+      cached_reason_file = session.staged_root.join("job_exit_reason")
+      if File.exists?(cached_reason_file)
+        File.open(cached_reason_file).read
+      else
+        reason = find_exit_reason(log_file)
+        File.open(cached_reason_file, 'w') { |file| file.write(reason) }
+        reason
+      end
+    rescue IOError
+      ""
+    end
+
+    # Parse the last lines of log_file and find common exit reasons
+    def find_exit_reason(log_file)
+      if File.exists?(log_file)
+        lines, _ = Open3.capture2("tail", "-n 9", log_file.to_s)
+        # Slurm time limit
+        if lines.include?("DUE TO TIME LIMIT")
+          "Job exceeded time limit"
+        # Slurm memory limit
+        elsif lines.include?("Exceeded job memory limit")
+          "Job exceeded memory limit"
+        # Cancel via scancel
+        elsif lines.include?("CANCELLED AT")
+          "Job was cancelled"
+        # App didn't launch in the time specified in after.sh
+        elsif lines.include?("Timed out waiting")
+          "Job took too long to start"
+        else
+          "Job cancelled for unknown reason"
+        end
+      else
+        "Log file for job does not exist"
+      end
+    end
+
   end
 
 end
