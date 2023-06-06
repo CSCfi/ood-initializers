e5100569913c1856ee8ebec5b02aec3721c90f72
Robin Karlsson <robin.karlsson@csc.fi>
2023-06-06 15:59:45 +0300

    Add accessility statement and cookie modals
    Show modals within dashboard instead of redirecting.
    Load modal code from ood-html assets.

---
+++ javascript/dashboard_modals.js
@@ -0,0 +1,17 @@
+import {
+  setModals,
+  toggleModal
+} from "/assets/modules/modals.js";
+
+$(function() {
+  const container = document.createElement("div");
+  container.id = "parent-container";
+  document.body.appendChild(container);
+  setModals();
+
+  const cookiePolicyButton = document.getElementById("cookie-policy-toggle");
+  cookiePolicyButton.addEventListener("click", () => toggleModal("cookie-modal"));
+
+  const accessibilityStatementButton = document.getElementById("accessibility-statement-toggle");
+  accessibilityStatementButton.addEventListener("click", () => toggleModal("accessibility-modal"));
+})
