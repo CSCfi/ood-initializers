f7d019851ffdb6a9d6398ff8d0c7c9413048ce9d
Robin Karlsson <robin.karlsson@csc.fi>
2023-08-22 09:39:30 +0300

    Read extra Rclone config
    Reads $HOME/.config/rclone/ood.conf for Rclone. Remove when upstream PR
    is merged.

--- dashboard/rclone.rb
+++ dashboard/rclone.rb
@@ -18,6 +18,55 @@ class RcloneUtil
     rescue StandardError
       false
     end
+
+    def extra_conf
+      @@extra_env ||= begin
+          extra_conf = File.join(Dir.home, ".config/rclone/ood.conf")
+          o, e, s = rclone("config", "dump", env: { "RCLONE_CONFIG" => extra_conf })
+          return {} unless s.success?
+
+          remotes = JSON.parse(o)
+
+          # Combine config into a single Hash where keys and values are e.g.
+          # RCLONE_CONFIG_MYREMOTE_TYPE: "s3"
+          remotes.map do |remote_name, remote|
+            remote.transform_keys do |key|
+              "RCLONE_CONFIG_#{remote_name.upcase}_#{key.upcase}"
+            end
+          end.reduce(&:merge) || {} # reduce on empty array returns nil
+        end
+    rescue => e
+      Rails.logger.error("Could not read extra Rclone configuration: #{e.message}")
+      {}
+    end
+
+    def rclone(*args, env: extra_conf, **kwargs)
+      # Using Open3.capture3 will use Open3Extensions.capture3 from OOD, which logs the
+      # sensitive env vars. Access singleton method directly instead.
+      Open3.singleton_method(:capture3).call(env, rclone_cmd, *args, **kwargs)
+    end
+
+    def rclone_popen(*args, stdin_data: nil, env: extra_conf, **kwargs, &block)
+      # Use -q to suppress message about config file not existing
+      # need it here as we check err.present?
+      Open3.popen3(env, rclone_cmd, "--quiet", *args) do |i, o, e, t|
+        if stdin_data
+          i.write(stdin_data)
+        end
+        i.close
+
+        err_reader = Thread.new { e.read }
+
+        yield o
+
+        o.close
+        exit_status = t.value
+        err = err_reader.value.to_s.strip
+        if err.present? || !exit_status.success?
+          raise RcloneError.new(exit_status.exitstatus), "Rclone exited with status #{exit_status.exitstatus}\n#{err}"
+        end
+      end
+    end
   end
 end
 
