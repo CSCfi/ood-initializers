a6c7e9d062ac437c3bd65c592be977cdca557b73
Robin Karlsson <robin.karlsson@csc.fi>
2025-03-31 09:48:14 +0300

    Remove duplicated CSCConfiguration

--- dashboard/ood.rb
+++ dashboard/ood.rb
@@ -48,28 +48,3 @@ Rails.application.config.after_initialize do
     Rails.logger.error("Failed to create home directory quota file: #{e.message}")
   end
 end
-
-module CSCConfiguration
-  class << self
-    def release_name
-      @@release ||= begin
-          version = File.read("/etc/ood/config/CSC_OOD_RELEASE")
-          "Release #{version}"
-        rescue
-          "develop"
-        end
-    end
-
-    def reset_favorite_paths
-      OodFilesApp.candidate_favorite_paths.tap do |paths|
-        paths.clear
-        # Add each user's project projappl and scratch directories to the
-        # file app as links.
-        projects = User.new.groups.map(&:name)
-        # Assuming that the directories are named like the projects.
-        paths.concat projects.filter_map { |p| FavoritePath.new(File.realpath("/projappl/#{p}")) if File.exist?("/projappl/#{p}") }
-        paths.concat projects.filter_map { |p| FavoritePath.new(File.realpath("/scratch/#{p}")) if File.exist?("/scratch/#{p}") }
-      end
-    end
-  end
-end
